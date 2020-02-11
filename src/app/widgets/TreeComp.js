c1c68b2c463530fbd9b789748d570faeecb1ae13
Othmane TOUJRI <otoujri@labvantage.com>
2020-02-11 17:28:37 +0100

    TreeComp customisation

---
+++ src/app/widgets/TreeComp.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import Paper from '@material-ui/core/Paper';
+import {
+  SelectionState,
+  IntegratedSelection,
+  TreeDataState,
+  CustomTreeData,
+} from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+  TableTreeColumn,
+} from '@devexpress/dx-react-grid-material-ui';
+
+import {
+  generateRows,
+  defaultColumnValues,
+} from '../../../demo-data/generator';
+
+const getChildRows = (row, rootRows) => {
+  const childRows = rootRows.filter(r => r.parentId === (row ? row.id : null));
+  return childRows.length ? childRows : null;
+};
+
+export default () => {
+  const [columns] = useState([
+    { name: 'name', title: 'Name' },
+    { name: 'gender', title: 'Gender' },
+    { name: 'city', title: 'City' },
+    { name: 'car', title: 'Car' },
+  ]);
+  const [data] = useState(generateRows({
+    columnValues: {
+      id: ({ index }) => index,
+      parentId: ({ index, random }) => (index > 0 ? Math.trunc((random() * index) / 2) : null),
+      ...defaultColumnValues,
+    },
+    length: 20,
+  }));
+  const [tableColumnExtensions] = useState([
+    { columnName: 'name', width: 300 },
+  ]);
+  const [defaultExpandedRowIds] = useState([0]);
+
+  return (
+    <Paper>
+      <Grid
+        rows={data}
+        columns={columns}
+      >
+        <SelectionState />
+        <TreeDataState
+          defaultExpandedRowIds={defaultExpandedRowIds}
+        />
+        <CustomTreeData
+          getChildRows={getChildRows}
+        />
+        <IntegratedSelection />
+        <Table
+          columnExtensions={tableColumnExtensions}
+        />
+        <TableHeaderRow />
+        <TableTreeColumn
+          for="name"
+          showSelectionControls
+          showSelectAll
+        />
+      </Grid>
+    </Paper>
+  );
+};
