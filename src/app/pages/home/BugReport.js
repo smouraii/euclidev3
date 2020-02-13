57cfb05365a0d7274d0690ff3892acd637f865bf
Othmane TOUJRI <otoujri@labvantage.com>
2020-02-13 17:30:43 +0100

    ModalForm

--- src/app/pages/home/BugReport.js
+++ src/app/pages/home/BugReport.js
@@ -1,25 +1,25 @@
 import React from "react";
-import {
-  Portlet,
-  PortletBody,
-} from "../../partials/content/Portlet";
-import Datatable from "../../widgets/Datatables"
-import datatableData from "./data/datatableData.json"
+import { Portlet, PortletBody } from "../../partials/content/Portlet";
+import Datatable from "../../widgets/Datatables";
+import datatableData from "./data/datatableData.json";
 import ModalForm from "../../widgets/ModalForm";
 
 export default function BugReport() {
-
   return (
     <>
-    <Portlet>
+      <Portlet>
         <PortletBody fit={true}>
-        
           <div className="row row-no-padding row-col-separator-x1">
-          <div>
-            <ModalForm/>
-            </div>
             <div className="col-xl-12">
-            
+              <div
+                style={{
+                  float: "right",
+                  marginRight: "20px",
+                  marginTop: "20px"
+                }}
+              >
+                <ModalForm />
+              </div>
               <Datatable
                 title="A Datatable "
                 data={datatableData}
@@ -29,12 +29,6 @@ export default function BugReport() {
           </div>
         </PortletBody>
       </Portlet>
-
     </>
-   );
-  }
-
-
-
-
-
+  );
+}
