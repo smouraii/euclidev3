e6553319224b26e5578d570d3fc9c5c8820278de
Othmane TOUJRI <otoujri@labvantage.com>
2020-09-10 01:49:57 +0100

    adding API to Select and modifs in NewRequest

--- src/app/pages/home/NewRequest.js
+++ src/app/pages/home/NewRequest.js
@@ -11,165 +11,149 @@ import useSWR from "swr";
 import Api from "../home/data/Api.json";
 
 export default function NewRequest(props) {
-
-  let emptyArr = [];
-  function Profile(display) {
-     const { selectValues, error } = useSWR(
-      `http://localhost:8088/EuclideV2/api/getSelectOptions?dc=com.euclide.sdc.RequestStatus&display=${display}`
-    );
-    if (error) return <div>failed to load</div>;
-    if (!selectValues) return <div>loading...</div>;
-    selectValues.map(el => emptyArr.push(el.name)) 
-    
-   
-  }
-  Api.steps.fields.map((api) => (
-      Profile(api.selectproperties.display)
-  ))
-
-
-  const [fieldsNamesObject, setFieldsNameObject] = React.useState(null);
-  const [validationObject, setValidationObject] = React.useState(null);
-  const fieldsNames = props.step.fields.map((field) => field.sdccolumnid);
-
-  const renderFields = (formik) => {
-    return props.step.fields.map((field) => {
-      switch (field.columntype) {
-        case "input":
-          return (
-            <FInput
-              key={field.sdccolumnid}
-              name={field.sdccolumnid}
-              label={field.columntitle}
-              readonly={field.readonly}
-              hidden={field.hidden}
-              instructionalText={field.columnInstructionalText}
-            />
-          );
-        case "select":
-          return (
-            <FSelect
-              name={field.sdccolumnid}
-              label={field.columntitle}
-              readonly={field.readonly}
-              hidden={field.hidden}
-              instructionalText={field.columnInstructionalText}
-              data={emptyArr}
-            />
-          );
-        case "date":
-          return (
-            <FDate
-              key={field.sdccolumnid}
-              name={field.sdccolumnid}
-              label={field.columntitle}
-              readonly={field.readonly}
-              hidden={field.hidden}
-              instructionalText={field.columnInstructionalText}
-            />
-          );
-        case "numeric":
-          return (
-            <FNumeric
-              key={field.sdccolumnid}
-              name={field.sdccolumnid}
-              label={field.columntitle}
-              readonly={field.readonly}
-              hidden={field.hidden}
-              instructionalText={field.columnInstructionalText}
-              formik={formik}
-            />
-          );
-        default:
-          return null;
-      }
-    });
-  };
-
-  // input, select, auto, numeric, date
-
-  React.useEffect(() => {
-    const objToFill = {};
-    fieldsNames.forEach((fieldName) => (objToFill[fieldName] = ""));
-    setFieldsNameObject(objToFill);
-
-    const validationObj = {};
-
-    props.step.fields
-      .map((field) => {
+    const [fieldsNamesObject, setFieldsNameObject] = React.useState(null);
+    const [validationObject, setValidationObject] = React.useState(null);
+    const fieldsNames = props.step.fields.map((field) => field.sdccolumnid);
+  
+    const renderFields = (formik) => {
+      return props.step.fields.map((field) => {
         switch (field.columntype) {
           case "input":
-            // case "auto":
-            return {
-              name: field.sdccolumnid,
-              validation: field.mandatory
-                ? Yup.string().required("Mandatory Field")
-                : Yup.string()
-            };
-          case "numeric":
-            return {
-              name: field.sdccolumnid,
-              validation: field.mandatory
-                ? Yup.number("Must be a number")
-                    .required("Mandatory Field")
-                    .typeError("Must be a number")
-                : Yup.number("Must be a number").typeError("Must be a number")
-            };
-
+            return (
+              <FInput
+                key={field.sdccolumnid}
+                name={field.sdccolumnid}
+                label={field.columntitle}
+                readonly={field.readonly}
+                hidden={field.hidden}
+                instructionalText={field.columnInstructionalText}
+              />
+            );
+          case "select":
+            return (
+              <FSelect
+                name={field.sdccolumnid}
+                label={field.columntitle}
+                readonly={field.readonly}
+                hidden={field.hidden}
+                instructionalText={field.columnInstructionalText}
+                display={field.selectproperties.display}
+              />
+            );
           case "date":
-            return {
-              name: field.sdccolumnid,
-              validation: field.mandatory
-                ? Yup.date("Must be a date").required("Mandatory Field")
-                : Yup.date("Must be a date")
-            };
-
+            return (
+              <FDate
+                key={field.sdccolumnid}
+                name={field.sdccolumnid}
+                label={field.columntitle}
+                readonly={field.readonly}
+                hidden={field.hidden}
+                instructionalText={field.columnInstructionalText}
+              />
+            );
+          case "numeric":
+            return (
+              <FNumeric
+                key={field.sdccolumnid}
+                name={field.sdccolumnid}
+                label={field.columntitle}
+                readonly={field.readonly}
+                hidden={field.hidden}
+                instructionalText={field.columnInstructionalText}
+                formik={formik}
+              />
+            );
           default:
             return null;
         }
-      })
-      .forEach((elem) => {
-        if (elem) {
-          validationObj[elem.name] = elem.validation;
-        }
       });
-
-    setValidationObject(validationObj);
-  }, []);
-
-  return (
-    <>
-      {props.hidden ||
-        (fieldsNamesObject && (
-          <div>
-            <Formik
-              initialValues={fieldsNamesObject}
-              validationSchema={Yup.object(validationObject)}
-              onSubmit={(val) => {
-                console.log("submitting.......");
-                console.log(val);
-                props.setCurrent(props.current + 1);
-              }}
-            >
-              {(formikProps) => (
-                <Form>
-                  {renderFields(formikProps)}
-                  {props.current > 0 && (
-                    <Button
-                      type="secondary"
-                      onClick={() => props.setCurrent(props.current - 1)}
-                    >
-                      Prev
+    };
+  
+    // input, select, auto, numeric, date
+  
+    React.useEffect(() => {
+      const objToFill = {};
+      fieldsNames.forEach((fieldName) => (objToFill[fieldName] = ""));
+      setFieldsNameObject(objToFill);
+  
+      const validationObj = {};
+  
+      props.step.fields
+        .map((field) => {
+          switch (field.columntype) {
+            case "input":
+              // case "auto":
+              return {
+                name: field.sdccolumnid,
+                validation: field.mandatory
+                  ? Yup.string().required("Mandatory Field")
+                  : Yup.string()
+              };
+            case "numeric":
+              return {
+                name: field.sdccolumnid,
+                validation: field.mandatory
+                  ? Yup.number("Must be a number")
+                      .required("Mandatory Field")
+                      .typeError("Must be a number")
+                  : Yup.number("Must be a number").typeError("Must be a number")
+              };
+  
+            case "date":
+              return {
+                name: field.sdccolumnid,
+                validation: field.mandatory
+                  ? Yup.date("Must be a date").required("Mandatory Field")
+                  : Yup.date("Must be a date")
+              };
+  
+            default:
+              return null;
+          }
+        })
+        .forEach((elem) => {
+          if (elem) {
+            validationObj[elem.name] = elem.validation;
+          }
+        });
+  
+      setValidationObject(validationObj);
+    }, []);
+  
+    return (
+      <>
+        {props.hidden ||
+          (fieldsNamesObject && (
+            <div>
+              <Formik
+                initialValues={fieldsNamesObject}
+                validationSchema={Yup.object(validationObject)}
+                onSubmit={(val) => {
+                  console.log("submitting.......");
+                  console.log(val);
+                  props.setCurrent(props.current + 1);
+                }}
+              >
+                {(formikProps) => (
+                  <Form>
+                    {renderFields(formikProps)}
+                    {props.current > 0 && (
+                      <Button
+                        type="secondary"
+                        onClick={() => props.setCurrent(props.current - 1)}
+                      >
+                        Prev
+                      </Button>
+                    )}
+                    <Button htmlType="submit" type="primary">
+                      Next
                     </Button>
-                  )}
-                  <Button htmlType="submit" type="primary">
-                    Next
-                  </Button>
-                </Form>
-              )}
-            </Formik>
-          </div>
-        ))}
-    </>
-  );
-}
+                  </Form>
+                )}
+              </Formik>
+            </div>
+          ))}
+      </>
+    );
+  }
+  
--- src/app/widgets/inputs/FSelect.js
+++ src/app/widgets/inputs/FSelect.js
@@ -1,30 +1,49 @@
 import React from "react";
 import { Field, ErrorMessage } from "formik";
 import { Select } from "antd";
+import redaxios from "redaxios";
+import useSWR from "swr";
 
 export default function FSelect(props) {
+  const { data } = useSWR(
+    `http://localhost:8088/EuclideV2/api/getSelectOptions?dc=com.euclide.sdc.RequestStatus&display=${props.display}`
+  );
+
+  // React.useEffect(() => {
+  //   console.log("props", props);
+  //   redaxios
+  //     .get(
+  //       `http://localhost:8088/EuclideV2/api/getSelectOptions?dc=com.euclide.sdc.RequestStatus&display=${display}`
+  //     )
+  //     .then((res) => setData(res.data));
+  // }, []);
+
   return (
     <>
-      <label htmlFor={props.name}>{props.label}</label>
-      <Field
-        component={Select}
-        name={props.name}
-        placeholder={props.label}
-        disabled={props.readonly}
-        hidden={props.hidden}
-        style={{ width: "100%" }}
-      >
-        {/* {props.data.map((elem) => (
-          <Select.Option key={elem.id} value={elem.name}>
-            {elem.name}
-          </Select.Option>
-        ))} */}
-      </Field>
-      <p style={{ margin: 0 }}>{props.instructionalText}</p>
-      <ErrorMessage
-        name={props.name}
-        render={(msg) => <span style={{ color: "red" }}>{msg}</span>}
-      />
+      {data && (
+        <div>
+          <label htmlFor={props.name}>{props.label}</label>
+          <Field
+            component={Select}
+            name={props.name}
+            placeholder={props.label}
+            disabled={props.readonly}
+            hidden={props.hidden}
+            style={{ width: "100%" }}
+          >
+            {data.map((elem) => (
+              <Select.Option key={elem.id} value={elem.name}>
+                {elem.name}
+              </Select.Option>
+            ))}
+          </Field>
+          <p style={{ margin: 0 }}>{props.instructionalText}</p>
+          <ErrorMessage
+            name={props.name}
+            render={(msg) => <span style={{ color: "red" }}>{msg}</span>}
+          />
+        </div>
+      )}
     </>
   );
 }
