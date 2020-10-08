7231279572820b039c870f3865f47a813f884a30
Othmane TOUJRI <otoujri@labvantage.com>
2020-10-09 00:43:56 +0100

    adding sorting and PieChart API

--- src/app/widgets/DatatableFolderList.js
+++ src/app/widgets/DatatableFolderList.js
@@ -232,6 +232,7 @@ export default function Datatable() {
       title: column.title,
       dataIndex: column.data,
       key: column.name,
+      ...getColumnSearchProps(column.data),
       sorter: (a, b) =>
         a instanceof String || null
           ? a.column.data.value.localeCompare(b.column.data.value)
@@ -249,84 +250,7 @@ export default function Datatable() {
     console.log("mapColumns", mapColumns);
   }, [columnsData, userInfo]);
 
-  // render() {
-  // if (userInfo === null) {
-  //   const columns = [
-  //     {
-  //       title: "Id",
-  //       dataIndex: "id",
-  //       key: "id",
-  //       render: (id, val) => (
-  //         <Button
-  //           type="link"
-  //           onClick={() => handleChangeId(val)}
-  //         >{`${id.value}`}</Button>
-  //       ),
-  //       sorter: (a, b) => a.id.value - b.id.value,
-  //     },
-  //     {
-  //       title: "Request id",
-  //       dataIndex: "s_requestid",
-  //       key: "s_requestid",
-  //       sorter: (a, b) => a.s_requestid.localeCompare(b.s_requestid),
-  //       width: "20%",
-  //       ...getColumnSearchProps("s_requestid"),
-  //     },
-  //     {
-  //       title: "Creation Date",
-  //       dataIndex: "createdt",
-  //       key: "createdt",
-  //       defaultSortOrder: "descend",
-  //       //dob is date of birth from api
-  //       //a b used to sort from big to small
-  //       sorter: (a, b) => a.createdt - b.createdt,
-  //       ...getColumnSearchPropsDate("createdt"),
-  //     },
-  //     {
-  //       title: "Request status",
-  //       dataIndex: "requeststatus",
-  //       key: "requeststatus",
-  //       sorter: (a, b) => a.requeststatus.length - b.requeststatus.length,
-  //       filters: [
-  //         { text: "Draft", value: "draft" },
-  //         { text: "Open", value: "open" },
-  //       ],
-  //       onFilter: (value, record) => record.requeststatus.indexOf(value) === 0,
-  //     },
-  //     {
-  //       title: "Request description",
-  //       dataIndex: "requestdesc",
-  //       key: "requestdesc",
-  //       defaultSortOrder: "descend",
-  //       //dob is date of birth from api
-  //       //a b used to sort from big to small
-  //       sorter: (a, b) => a.requestdesc - b.requestdesc,
-  //       ...getColumnSearchProps("requestdesc"),
-  //     },
-  //     {
-  //       title: "Address description",
-  //       dataIndex: "addressdesc",
-  //       key: "addressdesc",
-  //       sorter: (a, b) => a.addressdesc.localeCompare(b.addressdesc),
-  //       ...getColumnSearchProps("addressdesc"),
-  //     },
-  //     {
-  //       title: "Address Type",
-  //       dataIndex: "addresstype",
-  //       key: "addresstype",
-  //       sorter: (a, b) => a.addresstype.localeCompare(b.addresstype),
-  //       ...getColumnSearchProps("addresstype"),
-  //     },
-  //     {
-  //       title: "Attachment",
-  //       dataIndex: "attachment",
-  //       key: "attachment",
-  //       render: (id, val) => (
-  //         <ModalAttachementList/>
-  //       ),
-  //       sorter: (a, b) => a.id.value - b.id.value,
-  //     }
-  //   ];
+ 
 
   return (
     <>
@@ -336,7 +260,7 @@ export default function Datatable() {
         columns={columnsApi}
         dataSource={dataSource}
       />}
-      {userInfo &&         <div className="row row-no-padding row-col-separator-x1">
+      {userInfo &&  <div className="row row-no-padding row-col-separator-x1">
           <div className="col-xl-12">
             <Portlet>
               <PortletBody fit={true}>
@@ -382,148 +306,5 @@ export default function Datatable() {
       {console.log(dataSource)}
     </>
   );
-  // } else {
-  //   const data = [];
-  //   const columns1 = [
-  //     {
-  //       title: "Id",
-  //       dataIndex: "id",
-  //       key: "id",
-  //       render: (id) => `${id.value}`,
-  //       sorter: (a, b) => a.id.value - b.id.value,
-  //     },
-  //     {
-  //       title: "s_Request id",
-  //       dataIndex: "s_requestid",
-  //       key: "s_requestid",
-  //       sorter: (a, b) => a.s_requestid.localeCompare(b.s_requestid),
-  //       width: "20%",
-  //       ...getColumnSearchProps("s_requestid"),
-  //     },
-  //     {
-  //       title: "Created date",
-  //       dataIndex: "createdt",
-  //       key: "createdt",
-  //       defaultSortOrder: "descend",
-  //       //a b used to sort from big to small
-  //       sorter: (a, b) => a.createdt - b.createdt,
-  //     },
-  //     {
-  //       title: "Request status",
-  //       dataIndex: "requeststatus",
-  //       key: "requeststatus",
-  //     },
-  //     {
-  //       title: "Request Description",
-  //       dataIndex: "requestdesc",
-  //       key: "requestdesc",
-  //       ...getColumnSearchProps("requestdesc"),
-  //     },
-  //     {
-  //       title: "Address desc",
-  //       dataIndex: "addressdesc",
-  //       key: "addressdesc",
-  //       ...getColumnSearchProps("addressdesc"),
-  //     },
-  //   ];
-  //   const columns2 = [
-  //     {
-  //       title: "Sample",
-  //       dataIndex: "sample",
-  //       key: "sample",
-  //       sorter: (a, b) => a.sample.localeCompare(b.sample),
-  //       width: "20%",
-  //       ...getColumnSearchProps("sample"),
-  //     },
-  //     {
-  //       title: "Description",
-  //       dataIndex: "description",
-  //       key: "description",
-  //       sorter: (a, b) => a.description.localeCompare(b.description),
-  //     },
-  //     {
-  //       title: "Status",
-  //       dataIndex: "status",
-  //       key: "	status",
-  //       sorter: (a, b) => a.status.localeCompare(b.status),
-  //       ...getColumnSearchProps("	status"),
-  //     },
-  //     {
-  //       title: "Request ID Evo",
-  //       dataIndex: "requestidevo",
-  //       key: "	requestidevo",
-  //       sorter: (a, b) => a.requestidevo.localeCompare(b.requestidevo),
-  //       ...getColumnSearchProps("	requestidevo"),
-  //     },
-  //   ];
-  //   const columns3 = [
-  //     {
-  //       title: "Sample",
-  //       dataIndex: "sample",
-  //       key: "sample",
-  //       render: (sample) => `${sample.value}`,
-  //       sorter: (a, b) => a.sample.value - b.sample.value,
-  //     },
-  //     {
-  //       title: "Test",
-  //       dataIndex: "test",
-  //       key: "test",
-  //       sorter: (a, b) => a.test.localeCompare(b.test),
-  //     },
-  //   ];
-
-  //   return (
-  //     <>
-  //       <div className="row row-no-padding row-col-separator-x1">
-  //         <div className="col-xl-12">
-  //           <Portlet>
-  //             <PortletBody fit={true}>
-  //               <PortletHeader title="Details" />
-  //               <Table
-  //                 style={{ backgroundColor: "white", padding: 20 }}
-  //                 columns={columns1}
-  //                 rowKey={(record) => record.login.uuid}
-  //                 dataSource={[userInfo]}
-  //                 // pagination={pagination}
-  //                 loading={loading}
-  //                 onChange={handleTableChange}
-  //               />
-  //             </PortletBody>
-  //           </Portlet>
-  //           <Portlet>
-  //             <PortletBody fit={true}>
-  //               <PortletHeader title="Sample" />
-  //               <Table
-  //                 style={{ backgroundColor: "white" }}
-  //                 columns={columns2}
-  //                 rowKey={(record) => record.login.uuid}
-  //                 dataSource={[userInfo]}
-  //                 // pagination={pagination}
-  //                 loading={loading}
-  //                 onChange={handleTableChange}
-  //               />
-  //             </PortletBody>
-  //           </Portlet>
-  //           <Portlet>
-  //             <PortletBody fit={true}>
-  //             <PortletHeader title="Results" />
-  //               <Table
-  //                 style={{ backgroundColor: "white" }}
-  //                 columns={columns3}
-  //                 rowKey={(record) => record.login.uuid}
-  //                 dataSource={[userInfo]}
-  //                 // pagination={pagination}
-  //                 loading={loading}
-  //                 onChange={handleTableChange}
-  //               />
-  //             </PortletBody>
-
-  //             {console.log(this)}
-  //           </Portlet>
-  // </div>
-  // </div>
-  // </>
-  // );
-  // }
-  // }
+ 
 }
